23b202a4dc9053e8a0485d29ebcb01cc04e44206
Ruchiiii-Gowda <ruchiiigowdaa@gmail.com>
2026-02-27 11:32:59 +0530

    initial clean commit

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,72 @@
+const Student = require("../models/Student");
+
+
+exports.createStudent = async (req, res) => {
+  try {
+
+    const student = new Student(req.body);
+    const savedStudent = await student.save();
+
+    res.status(201).json(savedStudent);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.getStudents = async (req, res) => {
+  try {
+
+    const students = await Student.find();
+
+    res.json(students);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+exports.getStudentById = async (req, res) => {
+  try {
+
+    const student = await Student.findById(req.params.id);
+
+    res.json(student);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.updateStudent = async (req, res) => {
+  try {
+
+    const student = await Student.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(student);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.deleteStudent = async (req, res) => {
+  try {
+
+    await Student.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "Student deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json(`{ message: error.message }`);
+  }
+};
